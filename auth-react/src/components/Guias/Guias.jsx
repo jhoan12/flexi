0736497d7b89af9e653eb788147bc9762ec5ce9c
alexis129 <jhoan.alvarez@models1a.com>
2022-02-04 14:22:31 -0500

    solucion de problemas buscador

--- auth-react/src/components/Guias/Guias.jsx
+++ auth-react/src/components/Guias/Guias.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllGuias } from "../../redux/actions/GuiasAction";
-import Buscador from "./Buscador";
 import "./guias.css";
 import TableGuias from "./TableGuias";
 const Guias = () => {
